adac49fc5a8229c946fb7474a1ec561bc9996ac5
Ezequiel Cortés <codelando@gmail.com>
2019-08-04 18:53:23 -0300

    HTML - Clase 4 - Repaso

--- html/clase-4/practica-1-kitchening/html/js/main.js
+++
@@ -1,9 +0,0 @@
-let toggleNav = document.querySelector(".toggle-nav");
-let mainNav = document.querySelector(".main-nav");
-
-console.log(toggleNav);
-console.log(mainNav);
-
-toggleNav.addEventListener('click', () => {
-  mainNav.classList.toggle('active');
-})
